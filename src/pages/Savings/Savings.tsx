cfe487f2f11c0b3c1ab28002aaea2ebe7a2070b7
Jennifer Yiu <jen.yiu.90@gmail.com>
2025-09-18 18:08:28 -0700

    cleanup file

--- src/pages/Savings/Savings.tsx
+++ src/pages/Savings/Savings.tsx
@@ -1,11 +1,6 @@
-import React from "react";
-import { Link, useNavigate } from "react-router-dom";
 import BackButton from "../../components/BackButton";
 
 const Savings = () => {
-  const navigate = useNavigate();
-
-  const handleGoBack = () => navigate(-1);
   return (
     <div>
       <BackButton />
